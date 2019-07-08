42db60500900bd0150cd04662fa4275f8365de5b
Mohamed Elbassiouny <apat4e@gmail.com>
2019-07-09 00:03:35 +0200

    app init

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Mohamed Elbassiouny on 7/8/19.
+//  Copyright © 2019 Mohamed Elbassiouny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
